b7fdd358f2b33a99ff6b86eca3a7f05c4d2d18e9
luizPrendin <luizprendinfelipe@gmail.com>
2023-09-14 17:23:22 -0300

    configurando as rotas

--- aula-rotas/src/main.jsx
+++ aula-rotas/src/main.jsx
@@ -2,14 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import{createBrowser,RouterProvider}from 'react-router-dom';
+import{createBrowser,createBrowserRouter,RouterProvider}from 'react-router-dom';
 import Home from './routes/Home/index.jsx';
 import Produtos from './routes/Produtos/index.jsx';
 import Erro from './routes/Error/index.jsx';
 import EditarProdutos from './routes/EditarProdutos/index.jsx';
 
+const router = createBrowserRouter([{
+  path: "/",
+  element: <App/>,
+  errorElement: <Erro/>,
+  children:[{
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/produtos",
+    element: <Produtos/>
+  },
+  {
+    path: "/produtos/editar/:id",
+    element: <EditarProdutos />
+  }
+]
+
+}])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
