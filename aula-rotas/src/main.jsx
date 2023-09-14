55e60eac8098a33927bf591221bdcf116b08c84c
luizPrendin <luizprendinfelipe@gmail.com>
2023-09-14 18:02:10 -0300

    add arquivo js listaProdutos

--- aula-rotas/src/main.jsx
+++ aula-rotas/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import{createBrowser,createBrowserRouter,RouterProvider}from 'react-router-dom';
+import{createBrowserRouter,RouterProvider}from 'react-router-dom';
 import Home from './routes/Home/index.jsx';
 import Produtos from './routes/Produtos/index.jsx';
 import Erro from './routes/Error/index.jsx';
